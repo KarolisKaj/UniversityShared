30af303a8d809a3a174c55fc45be912a3378c87a
KarolisKaj <karolis.kajenas1@gmail.com>
2018-02-18 21:51:28 +0200

    taking shape

--- Master's/Code/GenericModel/GenericModel/Simulation/Bootstrapper.py
+++ Master's/Code/GenericModel/GenericModel/Simulation/Bootstrapper.py
@@ -3,8 +3,8 @@ from Simulation.DisplayComponents.DataGrid import DataGrid
 class BootStrapper(object):
     def __init__(self):
         print("-----Initiliazing Bootstrapper-----")
-        dataGrid = DataGrid()
-        dataGrid.create_grid(100,self.run_sim_handle)
+        dataGrid = DataGrid(self.run_sim_handle)
+        dataGrid.create_grid(100, ['Unprocessed messages', 'Total messages'])
 
     def run_sim_handle(self):
         pass
--- Master's/Code/GenericModel/GenericModel/Simulation/DisplayComponents/DataGrid.py
+++ Master's/Code/GenericModel/GenericModel/Simulation/DisplayComponents/DataGrid.py
@@ -4,28 +4,36 @@ from matplotlib.widgets import Slider, Button
 from Simulation.DisplayComponents.DiscreteSlider import DiscreteSlider
 
 class DataGrid(object):
-    def __init__(self):
+    def __init__(self, run_sim_handle):
         print("Initializing DataGrid")
+        self.parameters = []
+        self.run_sim_handle = run_sim_handle;
 
-    def create_grid(self, maximumRange, run_sim_handle):
+    def create_grid(self, maximumRange, parameter_names):
         matplotlib.rcParams['figure.figsize'] = (13, 8)
 
         fig, ax = plt.subplots()
-        plt.subplots_adjust(left=0.10, bottom=0.20)
-        
-        components_axes = plt.axes([0.25, 0.05, 0.65, 0.03])
-        components_slider = DiscreteSlider(components_axes, 'Components count', 1, 10, valinit=4)
-        
-        p1, = ax.plot(range(maximumRange))
-        p2, = ax.plot(range(maximumRange))
+        plt.subplots_adjust(left=0.1, bottom=0.05)
 
-        p1.set_ydata(range(maximumRange))
-        p2.set_ydata(range(maximumRange))
+        for _ in parameter_names:
+            p1, = ax.plot(range(maximumRange))
+            self.parameters.append(p1) 
+
         fig.canvas.draw_idle()
-        components_slider.on_changed(run_sim_handle)
 
-        fig.legend([p1, p2], ['Unprocessed messages', 'Total messages'], loc='upper left')
+        fig.legend(self.parameters, parameter_names, loc='upper left')
         plt.show()
 
     def update_data(self, data):
-        pass
+        for i in range(data.Count()):
+            self.parameters[i].set_ydata(data[i])
+
+    def add_changeable_sliders():
+        components_axes = plt.axes([0.25, 0.05, 0.65, 0.03])
+        
+        components_slider = DiscreteSlider(components_axes, 'Components count', 1, 10, valinit=4)
+        components_slider.on_changed(self.run_sim_handle)
+
+        plt.subplots_adjust(left=0.1, bottom=0.20)
+
+
