7cdcf371ee8452a6d36532d84ec630ac1096292d
KarolisKaj <karolis.kajenas1@gmail.com>
2018-02-22 23:03:44 +0200

    exp

--- Master's/Code/GenericModel/GenericModel/ModelParser/EntryPoint.js
+++ Master's/Code/GenericModel/GenericModel/ModelParser/EntryPoint.js
@@ -1,27 +1,13 @@
 function runParsing()
 {
-    //var mermaid = require('./CoreParse/mermaid.min');
-    //console.log(mermaid.parse('graph TB'+
-    //'gamintojas--10--> buferis1'+
-    //'subgraph komponentas'+
-    //'buferis1[buferis]--> procesorius1[procesorius]'+
-    //'procesorius1[procesorius]--50--> procesorius1[procesorius]'+
-    //'end'+
-    //'procesorius1--15--> agregatorius'));
-    console.log('die');
-
+    console.log('Obtaining mermaid.');
     var mermaid = require('./mermaid');
-	    mermaid.initialize({ theme: 'forest' });
+	mermaid.initialize({ theme: 'forest' });
 
-//	console.log(mermaid.mermaidAPI);
-
-	console.log(mermaid.mermaidAPI.parse('graph TB\ngamintojas--10-->buferis1\nsubgraph komponentas\nbuferis1[buferis]-->procesorius1[procesorius]\nprocesorius1[procesorius]--50-->procesorius1[procesorius]\nend\nprocesorius1--15-->agregatorius\n'));
-//	console.log('We going strong');
-//    var flow = require('./flow');
-    //flow.parser.yy = graph;
-
-    //console.log('die');
-    //flow.parser.parse("");
+    console.log('Parsing given model.');
+    console.log(JSON.stringify(mermaid.parse('graph TB\ngamintojas--10-->buferis1\nsubgraph komponentas\nbuferis1[buferis]-->procesorius1[procesorius]\nprocesorius1[procesorius]--50-->procesorius1[procesorius]\nend\nprocesorius1--15-->agregatorius\n')));
+	console.log(mermaid.parse('graph TB\ngamintojas-->buferis1\nsubgraph komponentas\nbuferis1[buferis]-->procesorius1[procesorius]\nprocesorius1[procesorius]--50-->procesorius1[procesorius]\nend\nprocesorius1--15-->agregatorius\n').parser.yy.getEdges());
+    console.log(mermaid);
 }
 
 runParsing();
