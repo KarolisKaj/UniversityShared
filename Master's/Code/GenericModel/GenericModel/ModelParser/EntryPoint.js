f1771911728744a29a37b08771756ba13404ddb6
KarolisKaj <karolis.kajenas1@gmail.com>
2018-02-22 21:36:23 +0200

    Added model parsing from visual elements.

--- Master's/Code/GenericModel/GenericModel/ModelParser/EntryPoint.js
+++ Master's/Code/GenericModel/GenericModel/ModelParser/EntryPoint.js
@@ -10,8 +10,14 @@ function runParsing()
     //'procesorius1--15--> agregatorius'));
     console.log('die');
 
-    var graph = require('./graphDb');
-    var flow = require('./flow');
+    var mermaid = require('./mermaid');
+	    mermaid.initialize({ theme: 'forest' });
+
+//	console.log(mermaid.mermaidAPI);
+
+	console.log(mermaid.mermaidAPI.parse('graph TB\ngamintojas--10-->buferis1\nsubgraph komponentas\nbuferis1[buferis]-->procesorius1[procesorius]\nprocesorius1[procesorius]--50-->procesorius1[procesorius]\nend\nprocesorius1--15-->agregatorius\n'));
+//	console.log('We going strong');
+//    var flow = require('./flow');
     //flow.parser.yy = graph;
 
     //console.log('die');
