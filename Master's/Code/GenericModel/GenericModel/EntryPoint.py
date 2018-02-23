13e71686e24c1e176c91cbf3db439ec412d16aaf
KarolisKaj <karolis.kajenas1@gmail.com>
2018-02-23 22:51:50 +0200

    Interaction and parsing of model is now in place.

--- Master's/Code/GenericModel/GenericModel/EntryPoint.py
+++ Master's/Code/GenericModel/GenericModel/EntryPoint.py
@@ -1,3 +1,23 @@
+import argparse
+import json
+
+def addArgs():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('vertices')
+    parser.add_argument('edges')
+    parser.add_argument('subGraphs')
+    return parser;
+
+def getModelGraph(parser):
+    args = parser.parse_args()
+    vertices = json.loads(args.vertices)
+    edges = json.loads(args.edges)
+    subGraphs = json.loads(args.subGraphs)
+    return vertices, edges, subGraphs
+
+argsParser = addArgs()
+getModelGraph(argsParser)
+
 from Simulation.SimulationBootStrapper import SimulationBootStrapper
 
 bootstrapper = SimulationBootStrapper()
--- Master's/Code/GenericModel/GenericModel/ModelParser/EntryPoint.js
+++ Master's/Code/GenericModel/GenericModel/ModelParser/EntryPoint.js
@@ -1,13 +1,34 @@
-function runParsing()
-{
-    console.log('Obtaining mermaid.');
+function runParsing() {
+    console.log('Creating Mermaid Instance...');
     var mermaid = require('./mermaid');
-	mermaid.initialize({ theme: 'forest' });
+    mermaid.initialize({ theme: 'forest' });
+    console.log('Mermaid Instance Created!');
 
-    console.log('Parsing given model.');
-    console.log(JSON.stringify(mermaid.parse('graph TB\ngamintojas--10-->buferis1\nsubgraph komponentas\nbuferis1[buferis]-->procesorius1[procesorius]\nprocesorius1[procesorius]--50-->procesorius1[procesorius]\nend\nprocesorius1--15-->agregatorius\n')));
-	console.log(mermaid.parse('graph TB\ngamintojas-->buferis1\nsubgraph komponentas\nbuferis1[buferis]-->procesorius1[procesorius]\nprocesorius1[procesorius]--50-->procesorius1[procesorius]\nend\nprocesorius1--15-->agregatorius\n').parser.yy.getEdges());
-    console.log(mermaid);
+
+    console.log('Reading Model From File...');
+    const fs = require('fs');
+    var contents = fs.readFileSync('../Systems.Diagrams/MapReduce/SimplifiedExample.txt', 'utf8');
+    console.log('Model read!');
+    console.log("-----------------MODEL--------------------");
+    console.log(contents);
+    console.log("-----------------MODEL--------------------");
+
+    console.log('Parsing Model...');
+    var parsedGraph = mermaid.parse(contents);
+    var vertices = JSON.stringify(parsedGraph.parser.yy.getVertices());
+    var edges = JSON.stringify(parsedGraph.parser.yy.getEdges());
+    var subGraphs = JSON.stringify(parsedGraph.parser.yy.getSubGraphs());
+    console.log('Model Parsed!');
+
+    console.log('Running Python Generic Model...');
+    const { execFile } = require('child_process');
+    execFile('C:/Program Files (x86)/Microsoft Visual Studio/Shared/Python36_64/python.exe', ['C:/Users/WarHorse/SourceControl/UniversityShared/Master\'s/Code/GenericModel/GenericModel/EntryPoint.py', vertices, edges, subGraphs], (error, stdout, stderr) => {
+        if (error) {
+            console.log('Failed to run python file.');
+            throw error;
+        }
+        console.log(stdout);
+    });
 }
 
 runParsing();
