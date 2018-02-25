c9203da4795c621428cdb3a0227820c08d08afa9
KarolisKaj <karolis.kajenas1@gmail.com>
2018-02-25 23:56:12 +0200

    moving forwardino

--- Master's/Code/GenericModel/GenericModel/Simulation/Components/Component.py
+++ Master's/Code/GenericModel/GenericModel/Simulation/Components/Component.py
@@ -1,13 +1,13 @@
 class Component(object):
     def __init__(self, name):
         self.name = name;
-        print("Initializing " + self.name)
+        print("Initializing component named - " + self.name)
         self.actions = []
 
     def run(self, timeoutAction):
         while(true):
             for action in self.actions:
-                action()    
+                yield action()    
             yield timeoutAction
 
     def add_action(self, action):
--- Master's/Code/GenericModel/GenericModel/Simulation/SimulationBootStrapper.py
+++ Master's/Code/GenericModel/GenericModel/Simulation/SimulationBootStrapper.py
@@ -1,6 +1,7 @@
 import simpy
 
 from Simulation.DisplayComponents.DataGrid import DataGrid
+from Simulation.Components.Component import Component
 
 class SimulationBootStrapper(object):
     def __init__(self, vertices, edges, subgraphs):
@@ -14,20 +15,30 @@ class SimulationBootStrapper(object):
         self.dataGrid.create_grid(100, ['Unprocessed messages', 'Total messages'])
 
     def run_sim_handle(self):
+        components = self.initialize_components(self.edges)
+        components = self.add_component_actions(components, self.edges)
         self.env = simpy.Environment()
         self.store = simpy.Store(self.env)
+
         #TODO: Start monitoring;
         #TODO: Add parsing;
         #TODO: Add 
         pass
 
-    def initialize_components(self, components):
-        
-        #for edge in self.edges:
-        #    if(edge.)
-        #    pass
-        # TODO: Do the magic.
+    def initialize_components(self, edges):
+        components = []
+        for edge in edges:
+            if(not self.key_exists(edge["start"], components)):
+                components.append(Component(edge["start"]))
+            if(not self.key_exists(edge["end"], components)):
+                components.append(Component(edge["end"]))
+        return components
+
+    def key_exists(self, name, components):
+        for component in components:
+            if(name == component.get_name()):
+                return True
+        return False
+
+    def add_component_actions(self, components, edges):
         pass
-
-
-        
